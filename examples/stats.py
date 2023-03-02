5c91bb76ee1237439052e6c7919a9b7327e46372
Sasha Rush <srush.researech@gmail.com>
2023-03-02 09:04:58 -0500

    update

--- examples/stats.py
+++ examples/stats.py
@@ -7,21 +7,24 @@ from enum import Enum
 
 # Data specification
 
+# +
 class StatType(Enum):
     POINTS = 1
     REBOUNDS = 2
     ASSISTS = 3
-
+    
 @dataclass
 class Stat:
     value: int
     stat: StatType
-
+    
 @dataclass
 class Player:
     player: str
     stats: List[Stat]
+# -
 
+    
 # Code
 
 class ExtractionPrompt(minichain.TypedTemplatePrompt):
@@ -37,3 +40,6 @@ with minichain.start_chain("stats") as backend:
 ExtractionPrompt().show({"passage": "Harden had 10 rebounds."},
                         '[{"player": "Harden", "stats": {"value": 10, "stat": 2}}]')
 
+# View the run log.
+
+minichain.show_log("bash.log")
